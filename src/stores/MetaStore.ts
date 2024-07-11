8c4a8037afc1800355da73d32bb0df4f58899412
Ahmed Atwa <aatwa86@gmail.com>
2024-07-11 20:32:54 +0100

    Update MetaStore.ts

--- src/stores/MetaStore.ts
+++ src/stores/MetaStore.ts
@@ -1,13 +1,17 @@
 import { defineStore } from "pinia";
-import { ref, readonly } from "vue";
+import { ref, readonly, computed } from "vue";
 
 export const useMetaStore = defineStore("metadata-store", () => {
-  const _title = ref("Nobel Quran");
+  const _mainTitle = ref("");
+  const _title = computed(() => {
+    return import.meta.env.VITE_APP_TITLE + " - " + _mainTitle;
+  });
+
   const _metaData = ref<any[]>([]);
   const pageTitle = readonly(_title);
   const metaData = readonly(_metaData);
 
-  const setPageTitle = (v: string) => (_title.value = v);
+  const setPageTitle = (v: string) => (_mainTitle.value = v);
   const setMetaData = (v: any[]) => (_metaData.value = v);
 
   return {
