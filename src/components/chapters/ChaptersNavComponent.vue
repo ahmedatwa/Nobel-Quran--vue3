acdc25c93c5cecbf89f931a3bb28b4eaa0d15eb3
Ahmed Atwa <aatwa86@gmail.com>
2024-07-19 19:33:00 +0100

    update

--- src/components/chapters/ChaptersNavComponent.vue
+++ src/components/chapters/ChaptersNavComponent.vue
@@ -75,8 +75,6 @@ const getSelectedVerse = async (id: number) => {
 watchEffect(async () => {
   if (props.manualIntersectingMode) {
     const intersectingData = props.manualIntersectingMode
-
-
     selectedVerseID.value = intersectingData.currentVerseNumber;
 
     if (selectedChapterVersesCount.value === intersectingData.lastVerseNumber) {
